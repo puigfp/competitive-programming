a10da859770e5aeb08c19cce3738bb61a7e0ab87
Francisco-Pierre Puig <puig.franciscopierre@gmail.com>
2019-10-26 20:38:57 +0200

    [peking/november-rain] add slow "solution"

--- peking/november-rain/solve.py
+++ peking/november-rain/solve.py
@@ -4,13 +4,15 @@ node = namedtuple("node", ["sum", "updates"])
 update = namedtuple("update", ["x", "y", "value"])
 
 
-def init_segment_tree(n):
+def init_segment_tree(l):
     size = 1
-    while size < n:
+    while size < len(l):
         size *= 2
-    tree = [node(0, deque())] * size + [
-        node(1, deque()) if i < n else node(0, deque()) for i in range(size)
-    ]
+    tree = (
+        [node(0, deque())] * size
+        + [node(elem, deque()) for elem in l]
+        + [node(0, deque())] * (size - len(l))
+    )
 
     for i in reversed(range(1, size)):
         tree[i] = node(tree[2 * i][0] + tree[2 * i + 1][0], deque())
@@ -41,6 +43,7 @@ def query(x, y, tree):
 
 def add_update(x, y, value, tree):
     tree[1].updates.append(update(x, y, value))
+
 
 def apply_updates(x0, y0, k, tree):
     while tree[k][1]:
@@ -73,8 +76,9 @@ def apply_updates(x0, y0, k, tree):
         apply_updates(z0, y0, 2 * k + 1, tree)
         tree[k] = node(tree[2 * k].sum + tree[2 * k + 1].sum, tree[k].updates)
 
+
 def test_segment_tree():
-    tree = init_segment_tree(6)
+    tree = init_segment_tree([1] * 6)
     assert 3 == query(0, 3, tree)
     assert 6 == query(0, 8, tree)
     assert 0 == query(6, 8, tree)
@@ -92,3 +96,100 @@ def test_segment_tree():
     assert 3 == query(0, 3, tree)
     assert 0 == query(0, 6, tree)
     assert 1 == query(1, 2, tree)
+
+
+def read_segments():
+    # reads problem input
+    n = int(input())
+    segments = list()
+    for _ in range(n):
+        xa, ha, xb, hb = map(int, input().split())
+        segments.append(((xa, ha), (xb, hb)))
+    return segments
+
+
+def is_above(segment1, segment2):
+    # returns True if segment1 is above segment2
+    # assumes x1 <= x <= x2
+    (x1, y1), (x2, y2) = segment1
+    (x, y) = segment2[0]
+    return (y2 - y1) / (x2 - x1) * (x - x1) + y1 >= y
+
+
+def test_is_above():
+    assert True == is_above(((3, 8), (7, 7)), ((5, 5), (9, 3)))
+    assert False == is_above(((1, 7), (5, 6)), ((3, 8), (7, 7)))
+
+
+def sort_segments(segments):
+    # topologically sort segment (if segment1 is above segment2, then segment1 will be
+    # before segment 2 in the sorted list)
+    ids = sorted(range(len(segments)), key=lambda i: segments[i])
+    adj = [[] for i in ids]
+    for i in range(len(ids)):
+        for j in range(i + 1, len(ids)):
+            if segments[ids[i]][1][0] < segments[ids[j]][0][0]:
+                break
+            if is_above(segments[ids[i]], segments[ids[j]]):
+                adj[ids[i]].append(ids[j])
+            else:
+                adj[ids[j]].append(ids[i])
+    sort = topological_sort(adj)
+    return sort
+
+
+def topological_sort(adj):
+    seen = set()
+    sort = []
+
+    def bfs(i):
+        if i in seen:
+            return
+        seen.add(i)
+
+        for j in adj[i]:
+            bfs(j)
+
+        sort.append(i)
+
+    for i in range(len(adj)):
+        bfs(i)
+
+    sort.reverse()
+    return sort
+
+def test_topological_sort():
+    adj =[
+        [],
+        [2, 3, 4, 0],
+        [4],
+        [2, 4],
+        [0],
+        [],
+    ]
+    assert [5, 1, 3, 2, 4, 0] == topological_sort(adj)
+
+
+def solve(segments):
+    x_max = max(max(elem[0][0], elem[1][0]) for elem in segments)
+    tree = init_segment_tree([0, 1] * (x_max + 1))
+    order = sort_segments(segments)
+    water = dict()
+    for i in order:
+        (x1, y1), (x2, y2) = segments[i]
+        s = query(x1 * 2, x2 * 2 + 1, tree)
+        water[i] = s
+        if y1 < y2:
+            add_update(2 * x1, 2 * x1 + 1, s, tree)
+            add_update(2 * x1 + 1, 2 * x2 + 1, 0, tree)
+        else:
+            add_update(2 * x2, 2 * x2 + 1, s, tree)
+            add_update(2 * x1, 2 * x2, 0, tree)
+
+    for i in sorted(water):
+        print(water[i])
+
+
+if __name__ == "__main__":
+    segments = read_segments()
+    solve(segments)
